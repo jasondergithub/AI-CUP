0b7462cc2e87a591ac87928b28b044ea726e7984
Jason Liu <j84yes116@gmail.com>
2021-11-26 22:13:40 +0800

    plus config

---
+++ main/config.py
@@ -0,0 +1,9 @@
+from transformers import BertConfig
+
+DEVICE = "cuda"
+EPOCHS = 10
+MODEL_PATH = "../saved_model"
+
+bert_config = BertConfig.from_pretrained('bert-base-chinese')
+bert_config.num_hidden_layers = 6
+bert_config.num_attention_heads = 6
---
+++ main/engine.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+def loss_fn(outputs, targets):
+    return nn.MSELoss()(outputs, targets)
+
+def train(data_loader, model, optimizer, device, scheduler):
+    model.train()
+
+    for  batch_index, data in tqdm(enumerate(data_loader), total=len(data_loader)):
+        if next(model.parameters()).is_cuda:
+             data = [t.to("cuda:0") for t in data if t is not None]
+        
+        tokens_tensors, segments_tensors, masks_tensors = data[:3]
+
+        optimizer.zero_grad()
+        outputs = model(input_ids=tokens_tensors, attention_mask=masks_tensors, token_type_ids=segments_tensors)
+        targets = model.bert_output
+
+        loss = loss_fn(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+def eval_fn(data_loader, model, device):
+    model.eval()
+
+    outpus_loss = []
+    with torch.no_grad():
+        pass
