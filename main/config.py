c81e7c5a54c69103df06f324dda46a979a0052f6
109598089 <t109598089@ntut.org.tw>
2021-11-27 21:34:47 +0800

    edit max position embeddings to 1000

--- main/config.py
+++ main/config.py
@@ -1,4 +1,5 @@
 from transformers import BertConfig, BertTokenizer
+from transformers.models import bert
 
 DEVICE = "cuda"
 EPOCHS = 10
@@ -7,5 +8,6 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
 MODEL_PATH = "../saved_model"
 
 bert_config = BertConfig.from_pretrained('bert-base-chinese')
+bert_config.max_position_embeddings = 1000
 bert_config.num_hidden_layers = 6
 bert_config.num_attention_heads = 6
