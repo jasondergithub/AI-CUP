b78a271b179b3b3e2cd0a1d1123c7b4feb90d4f7
109598089 <t109598089@ntut.org.tw>
2021-11-30 17:50:03 +0800

    loss between unrelated articles in training set

--- main/eval.py
+++ main/eval.py
@@ -6,39 +6,37 @@ import pandas as pd
 
 from bert_auto_encoder_base1 import Model
 from dataset import encoderDataset
-from torch.utils.data import DataLoader
-from torch.nn.utils.rnn import pad_sequence
 
 device = torch.device(config.DEVICE)
 model = Model()
 model.load_state_dict(torch.load(config.MODEL_PATH))
 model.to(device)
 
-trainset = encoderDataset('train', config.tokenizer)
-tokens_tensors, segments_tensors = trainset.__getitem__(0)
+# trainset = encoderDataset('train', config.tokenizer)
+# tokens_tensors, segments_tensors = trainset.__getitem__(0)
 
-masks_tensors = torch.zeros(tokens_tensors.shape, dtype=torch.long)
-masks_tensors = masks_tensors.masked_fill(tokens_tensors != 0, 1)
-tokens_tensors = tokens_tensors.unsqueeze(0)
-segments_tensors = segments_tensors.unsqueeze(0)
-masks_tensors = masks_tensors.unsqueeze(0)
+# masks_tensors = torch.zeros(tokens_tensors.shape, dtype=torch.long)
+# masks_tensors = masks_tensors.masked_fill(tokens_tensors != 0, 1)
+# tokens_tensors = tokens_tensors.unsqueeze(0)
+# segments_tensors = segments_tensors.unsqueeze(0)
+# masks_tensors = masks_tensors.unsqueeze(0)
 
-tokens_tensors = tokens_tensors.to(device)
-masks_tensors = masks_tensors.to(device)
-segments_tensors = segments_tensors.to(device)
+# tokens_tensors = tokens_tensors.to(device)
+# masks_tensors = masks_tensors.to(device)
+# segments_tensors = segments_tensors.to(device)
 
-model.eval()
-outputs = model(tokens_tensors=tokens_tensors,
-                segments_tensors=segments_tensors,
-                masks_tensors=masks_tensors)
-targets = model.bert_output[0]
-del model.bert_output[0]
+# model.eval()
+# outputs = model(tokens_tensors=tokens_tensors,
+#                 segments_tensors=segments_tensors,
+#                 masks_tensors=masks_tensors)
+# targets = model.bert_output[0]
+# del model.bert_output[0]
 
-loss = torch.nn.MSELoss()
-output = loss(outputs, targets)
-print(f'sample 0 loss = {output}')
+# loss = torch.nn.MSELoss()
+# output = loss(outputs, targets)
+# print(f'sample 0 loss = {output}')
 
-print('======================================================================================')
+# print('======================================================================================')
 
 with open('../processed_files/' + str(32) + '.txt', 'r', encoding='UTF-8') as text1:
     file1 = text1.read()
@@ -53,11 +51,8 @@ wordpieces += tokens2 + ['[SEP]']
 article2_len = len(wordpieces) - article1_len
 
 ids = config.tokenizer.convert_tokens_to_ids(wordpieces)
-tokens_tensor = torch.tensor(ids)
-segments_tensor = torch.tensor([0] * article1_len + [1] * article2_len, dtype=torch.long)
-trainset = encoderDataset('train', config.tokenizer)
-tokens_tensors, segments_tensors = trainset.__getitem__(0)
-
+tokens_tensors = torch.tensor(ids)
+segments_tensors = torch.tensor([0] * article1_len + [1] * article2_len, dtype=torch.long)
 masks_tensors = torch.zeros(tokens_tensors.shape, dtype=torch.long)
 masks_tensors = masks_tensors.masked_fill(tokens_tensors != 0, 1)
 tokens_tensors = tokens_tensors.unsqueeze(0)
