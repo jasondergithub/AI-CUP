05d9e6786c220539c11dee6bc724e1382398b9a9
109598089 <t109598089@ntut.org.tw>
2021-11-30 15:50:16 +0800

    fix bug

--- main/eval.py
+++ main/eval.py
@@ -16,8 +16,8 @@ model.to(device)
 
 trainset = encoderDataset('train', config.tokenizer)
 tokens_tensors, segments_tensors = trainset.__getitem__(0)
-tokens_tensors = pad_sequence(tokens_tensors, batch_first=True)                                      
-segments_tensors = pad_sequence(segments_tensors, batch_first=True)
+# tokens_tensors = pad_sequence(tokens_tensors, batch_first=True)                                      
+# segments_tensors = pad_sequence(segments_tensors, batch_first=True)
 masks_tensors = torch.zeros(tokens_tensors.shape, dtype=torch.long)
 masks_tensors = masks_tensors.masked_fill(tokens_tensors != 0, 1)
 
