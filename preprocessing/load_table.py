1d47a0515384efcadb5c07051f80018064103dbf
Jason Liu <j84yes116@gmail.com>
2021-12-04 23:52:39 +0800

    accept current change

--- preprocessing/load_table.py
+++ preprocessing/load_table.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pickle
 import random
 
@@ -15,21 +14,4 @@ for i in range(10):
     subtable = subtable + related_table
     random.shuffle(subtable)
     with open("../table/" + name_list[i], "wb") as fp:
-=======
-import pickle
-import random
-
-with open("../dict/relatedTable.txt", "rb") as fp:
-    related_table = pickle.load(fp)
-
-with open("../dict/unrelatedTable.txt", "rb") as fp:
-    unrelated_table = pickle.load(fp)
-
-name_list = ['table1.txt', 'table2.txt', 'table3.txt', 'table4.txt', 'table5.txt', 'table6.txt', 
-'table7.txt', 'table8.txt', 'table9.txt', 'table10.txt']
-for i in range(10):
-    subtable = random.sample(unrelated_table, 1381)
-    subtable = subtable + related_table
-    with open("../table/" + name_list[i], "wb") as fp:
->>>>>>> c81d0f349c5860990a39bd8d48aba1fc5488af12
         pickle.dump(subtable, fp)
